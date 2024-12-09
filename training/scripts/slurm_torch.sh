4a2bdb66e424cbb74fc81db461b63663ba150923
ignacio-scarinci <ignacio.scarinci@gmail.com>
2024-12-09 13:59:01 -0300

    algo

--- training/scripts/slurm_torch.sh
+++ training/scripts/slurm_torch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=tiny_prueba
-#SBATCH --nodes=2
+#SBATCH --job-name=tiny_prueba2
+#SBATCH --nodes=1
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=20
 #SBATCH --partition=multi
